e2bdf5e6b280a5d7c05bc6d3b436b65b5cfd53af
MalicknND <ndiayemalick062@gmail.com>
2024-03-26 13:13:31 +0100

    💄 Ready 👌🪄

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,22 @@ import Hero from "./components/Hero";
 import Features from "./components/Features";
 import Workflow from "./components/Workflow";
 import Pricing from "./components/Pricing";
+import Testimonials from "./components/Testimonials";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
     <>
       <Navbar />
-      <div className="lg:mx-40 sm:mx-20 md:mx-10">
+      <div className="lg:mx-40 md:mx-10">
         <div className="max-w-7xl mx-auto pt-20 px-6">
           <Hero />
         </div>
         <Features />
         <Workflow />
         <Pricing />
+        <Testimonials />
+        <Footer />
       </div>
     </>
   );
